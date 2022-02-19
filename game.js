bb24e06ba585cdaaa2c77fc678102770f5c53d26
Markko Junolainen <markko.junolainen@gmail.com>
2022-02-19 09:41:47 +0200

    new board

--- game.js
+++ game.js
@@ -1,5 +1,7 @@
 import { gameBoard, buildGameBoard } from './grid.js';
 
+const FASTEST_SPEED = 2;
+
 let lastRenderTime = 0;
 buildGameBoard();
 
@@ -8,5 +10,14 @@ function main(currentTime) {
   const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
   if (secondsSinceLastRender < 1 / FASTEST_SPEED) return;
   lastRenderTime = currentTime;
+
+  update();
+  render();
 }
 window.requestAnimationFrame(main);
+
+const pacMan = [{ x: 14, y: 24 }];
+
+function update() {}
+
+function render() {}
